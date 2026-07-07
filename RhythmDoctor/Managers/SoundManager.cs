c9906ce770b31ea95cba7cd902fdc5aa4831558c
SJT <tlawlxo78@gmail.com>
2026-07-07 17:17:39 +0900

    프로젝트 -> NuGet패키지 관리 -> System.Windows.Extensions 설치했음
    .net10 버전에서는 자동으로 System.Media.SoundPlayer를 지원하지 않는다고 함.
    그래서 NuGet 패키지에서  System.Windows.Extensions 를 설치해줘야한다고함

--- RhythmDoctor/Managers/SoundManager.cs
+++ RhythmDoctor/Managers/SoundManager.cs
@@ -1,11 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Media;
 
 namespace RhythmDoctor.Managers
 {
     public class SoundManager
     {
+        string path = "";
+        SoundPlayer bgmPlayer;
+
         #region 싱글톤 패턴 적용 및 생성자
         private static SoundManager instance;
 
@@ -26,5 +30,15 @@ namespace RhythmDoctor.Managers
         {
         }
         #endregion
+
+        public void Play()
+        {
+
+        }
+
+        public void PlayOneShot()
+        {
+
+        }
     }
 }
