ae8b321ce92ee545348ce057f6d25073bb8c11a6
SJT <tlawlxo78@gmail.com>
2026-07-07 13:06:30 +0900

    InputManager 제자
    InputManager 에 플레이어의 인풋을 받았는지 여부를 판단하는 HasInput bool 타입 프로퍼티를 선언.
    
    Listen() 메서드를 구현하여, 게임이 진행되는동안 (= 게임오버가 아니면) 키보드의 키 입력을 받으면 HasInput이 true가 되도록 구현했음

--- RhythmDoctor/Managers/InputManager.cs
+++ RhythmDoctor/Managers/InputManager.cs
@@ -4,7 +4,45 @@ using System.Text;
 
 namespace RhythmDoctor.Managers
 {
-    internal class InputManager
+    public class InputManager
     {
+        #region 싱글톤 패턴 적용
+        private static InputManager instance;
+
+        public static InputManager Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new InputManager();
+                }
+
+                return instance;
+            }
+        }
+        #endregion
+
+        // 플레이어의 
+        public void Listen()
+        {
+            GameManager gm = GameManager.Instance;
+
+            while (!gm.IsGameOver) // 게임 오버가 되기 전까지 실행
+            {
+                // 키 입력이 없는데 Console.ReadKey를 바로 실행하면 프로그램이 키 입력을 기다리며 멈춰버림
+                if (Console.KeyAvailable) // 현재 콘솔에 읽을 수 있는 키 입력이 있는지 확인
+                {
+                    ConsoleKeyInfo keyInfo = Console.ReadKey(true); // 입력된 키 정보를 읽고, 화면에는 표시하지 않음
+
+                    if (keyInfo.Key == ConsoleKey.Spacebar || keyInfo.Key == ConsoleKey.B) // 입력된 키가 스페이스바인지 확인
+                    {
+                        HasInput = true; // 스페이스바 입력이 들어왔음을 표시
+                    }
+                }
+            }
+        }
+
+        public bool HasInput { get; set; } // 유저의 인풋이 들어왔는지 여부를 나타내는 bool 프로퍼티
     }
 }
