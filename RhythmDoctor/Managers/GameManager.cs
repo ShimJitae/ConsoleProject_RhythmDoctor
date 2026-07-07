16aab0425c8b5a4f510cca6764c87caedd0e8341
SJT <tlawlxo78@gmail.com>
2026-07-07 12:46:08 +0900

    GameManager 제작
    게임의 진행 상태를 관리하는 GameManager 클래스 제작
    
    게임 플레이 중인지 여부를 나타내는 bool 프로퍼티 IsGameOver 선언

---
+++ RhythmDoctor/Managers/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using RhythmDoctor.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RhythmDoctor.Managers
+{
+    public class GameManager
+    {
+        #region 싱글톤 패턴 적용
+        private static GameManager instance;
+
+        public static GameManager Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new GameManager();
+                }
+
+                return instance;
+            }
+        }
+        #endregion
+
+        public bool IsGameOver { get; set; }
+    }
+}
