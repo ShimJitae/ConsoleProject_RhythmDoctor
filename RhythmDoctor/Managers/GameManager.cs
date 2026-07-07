a05200adced69bcd8aed9fa3c35894a86ac9a76d
SJT <tlawlxo78@gmail.com>
2026-07-07 13:15:38 +0900

    GameManager에 생성자 추가 및 StartGame 메서드 생성
    GameManager에 생성자 추가 및 StartGame 메서드 생성

--- RhythmDoctor/Managers/GameManager.cs
+++ RhythmDoctor/Managers/GameManager.cs
@@ -7,7 +7,7 @@ namespace RhythmDoctor.Managers
 {
     public class GameManager
     {
-        #region 싱글톤 패턴 적용
+        #region 싱글톤 패턴 적용 및 생성자
         private static GameManager instance;
 
         public static GameManager Instance
@@ -22,8 +22,20 @@ namespace RhythmDoctor.Managers
                 return instance;
             }
         }
+
+        private GameManager()
+        {
+            // 게임 오버 프로퍼티는 true로 시작함.
+            // IsGameOver는 게임이 실행하는 동안에만 false로 설정
+            IsGameOver = true;
+        }
         #endregion
 
         public bool IsGameOver { get; set; }
+
+        public void StartGame()
+        {
+            IsGameOver = false;
+        }
     }
 }
