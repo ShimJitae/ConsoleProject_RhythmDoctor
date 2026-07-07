b5d079b1fd6d5bd4cf17dc5961806b81dc6427c8
SJT <tlawlxo78@gmail.com>
2026-07-07 11:18:18 +0900

    리듬 매니저 초안 작성
    구현된 기능
    1. 현재 플레이할 음원의 박자감을 설정하는 메서드
    2. 한 마디를 16개의 반의 반박자로 나누어 감지하는 메서드

---
+++ RhythmDoctor/Managers/RhythmManager.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace RhythmDoctor.Managers
+{
+    public class RhythmManager
+    {
+        /// <summary>
+        /// 현재 플레이할 음악의 리듬 타이밍을 세팅하는 단계
+        /// 게임을 시작하기 전에 이 메서드를 실행시켜서 몇 bpm인지 설정해줘야함.
+        /// SetRhythm() 이후에 PlayOneMeasure()를 반복하면서 게임을 진행함
+        /// </summary>
+        int bpm = 0;
+        double oneBeatTime = 0;
+        double sixteenthBeatTime = 0;
+        double targetTime = 0;
+        public void SetRhythm(int _bpm = 94)
+        {
+            // 현재 목표는 bpm 94
+            bpm = _bpm;
+            oneBeatTime = 60.0 / bpm;
+            sixteenthBeatTime = oneBeatTime / 4.0;
+            targetTime = 0;
+        }
+
+        /// <summary>
+        /// 한 마디에 반의 반박자 16개가 있음
+        /// PlayOneMeasure()에서는 한 박자를 기준으로 만들어진 메서드
+        /// for문에서 한 번에 반의 반박자 시간만큼 시간을 감지하고, 이를 16번 반복
+        /// </summary>
+        public void PlayOneMeasure()
+        {
+            Stopwatch stopwatch = Stopwatch.StartNew();
+
+            for (int i = 0; i < 16; i++)
+            {
+                targetTime = sixteenthBeatTime * i;
+
+                while (stopwatch.Elapsed.TotalSeconds < targetTime)
+                {
+                    // 반의 반박자 만큼 멈췄다 진행
+                }
+
+                Console.WriteLine($"{i + 1}번째 반의 반박자");
+            }
+        }
+    }
+}
