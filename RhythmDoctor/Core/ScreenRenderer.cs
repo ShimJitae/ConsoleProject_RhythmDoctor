d307134c39e527a3fcc988300ae4676da35e5bb1
ShimJitae <99947889+ShimJitae@users.noreply.github.com>
2026-07-08 11:08:10 +0900

    RenderingData 구성
    RenderingData 구성
    -이미지 이름
    -렌더링 타입
    -이미지에서 그리기 시작할 피벗
    -이미지를 그릴 위치
    
    피벗과 위치를 표현하기 위해서 Vector2 구조체를 구현했음

--- RhythmDoctor/Core/ScreenRenderer.cs
+++ RhythmDoctor/Core/ScreenRenderer.cs
@@ -11,10 +11,6 @@ namespace RhythmDoctor.Core
         /// (int row, int col) pivot -> 이미지 제작을 시작할 피벗. 여기서부터 설정된 범위까지 문자열을 그려낸다.
         /// </summary>
         /// <param name="pivot"></param>
-        public void Render((int row, int col) pivot)
-        {
-
-        }
 
         StringBuilder sb_Background = new StringBuilder();
         StringBuilder sb_TimingBar = new StringBuilder();
@@ -29,9 +25,46 @@ namespace RhythmDoctor.Core
         {
             return "";
         }
+
+        public void Render((int row, int col) pivot)
+        {
+
+        }
     }
 
-    enum RenderLayer
+    public class RenderingData
+    {
+        public RenderLayer RenderType { get; set; }
+        public Vector2 RenderingPivot { get; set; }
+        public Vector2 RenderingPosition { get; set; }
+
+        public RenderingData(
+            string imageName, RenderLayer _RenderType, 
+            int RenderingPivot_Row = 0, int RenderingPivot_Col = 0, 
+            int RenderingPosition_Row = 0, int RenderingPosition_Col = 0
+            )
+        {
+            // 여기에 imageName으로 이미지 로드
+
+            RenderingPivot = new Vector2(RenderingPivot_Row, RenderingPivot_Col);
+            RenderingPosition = new Vector2(RenderingPosition_Row, RenderingPosition_Col);
+            RenderType = _RenderType;
+        }
+    }
+
+    public struct Vector2
+    {
+        public float x;
+        public float y;
+
+        public Vector2(int _y, int _x)
+        {
+            y = _y;
+            x = _x;
+        }
+    }
+
+    public enum RenderLayer
     {
         Background, // 게임의 뒷 배경
         TimingBar, // 플레이어가 입력을 넣을 타이밍을 보여주는 타이밍바
