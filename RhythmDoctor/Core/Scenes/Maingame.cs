419143e9094a64c91f27545137912b92f2d2205b
ShimJitae <99947889+ShimJitae@users.noreply.github.com>
2026-07-08 10:00:49 +0900

    씬 제작 시작
    씬 매니저
    
    씬은 추상클래스로 구성하였고,
    각각의 씬에서 StartScene을 상속받는다

---
+++ RhythmDoctor/Core/Scenes/Maingame.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RhythmDoctor.Core.Scenes
+{
+    public class Maingame : Scene
+    {
+        public override void StartScene()
+        {
+        }
+    }
+}
---
+++ RhythmDoctor/Core/Scenes/Scene.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RhythmDoctor.Core.Scenes
+{
+    public enum ScnenType
+    {
+        Title,
+        Maingame,
+        Result
+    }
+
+    public abstract class Scene
+    {
+        public abstract void StartScene();
+    }
+}
