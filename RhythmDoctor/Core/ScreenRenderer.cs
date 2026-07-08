a52c33f9c3d4d28c45b6d42a1bfa8b06cce2a5d9
ShimJitae <99947889+ShimJitae@users.noreply.github.com>
2026-07-08 10:36:31 +0900

    ScreenRenderer 제작
    ScreenRenderer 제작 시작

---
+++ RhythmDoctor/Core/ScreenRenderer.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RhythmDoctor.Core
+{
+    // 실제 화면에 이미지(텍스트 파일)을 렌더링 해주는 코어
+    public class ScreenRenderer
+    {
+        /// <summary>
+        /// (int row, int col) pivot -> 이미지 제작을 시작할 피벗. 여기서부터 설정된 범위까지 문자열을 그려낸다.
+        /// </summary>
+        /// <param name="pivot"></param>
+        public void Render((int row, int col) pivot)
+        {
+
+        }
+
+        StringBuilder sb_Background = new StringBuilder();
+        StringBuilder sb_TimingBar = new StringBuilder();
+        StringBuilder sb_Animation = new StringBuilder();
+        StringBuilder sb_UI = new StringBuilder();
+
+        /// <summary>
+        /// 실제 이미지를 로드해오는 메서드
+        /// </summary>
+        /// <returns></returns>
+        string LoadImage()
+        {
+            return "";
+        }
+    }
+
+    enum RenderLayer
+    {
+        Background, // 게임의 뒷 배경
+        TimingBar, // 플레이어가 입력을 넣을 타이밍을 보여주는 타이밍바
+        Animation, // 타이밍 바 
+        UI
+    }
+}
