7cb988b9348dc228e25d348577a11946c5af9fc6
ReversePI78 <99947889+ShimJitae@users.noreply.github.com>
2026-07-09 22:44:27 +0900

    activerendering 이벤트 추가
    activerendering 이벤트 추가

---
+++ RhythmDoctor/Core/BeatEvents/ActiveRendering.cs
@@ -0,0 +1,24 @@
+﻿using RhythmDoctor.Managers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RhythmDoctor.Core.BeatEvents
+{
+    public class ActiveRendering : BeatEvent
+    {
+        RenderLayer target;
+        bool enabled;
+
+        public override void Play()
+        {
+            CameraManager.Instance.ActiveRendering(target, enabled);
+        }
+
+        public ActiveRendering(RenderLayer _target, bool _enabled)
+        {
+            target = _target;
+            enabled = _enabled;
+        }
+    }
+}
